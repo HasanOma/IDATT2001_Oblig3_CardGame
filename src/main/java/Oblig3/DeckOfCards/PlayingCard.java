8ed47369afcf3c86119592c8af7bb14c69d5ed13
hasanro <hasanro@stud.ntnu.no>
2021-03-22 13:29:25 +0100

    Constructor and Getter methods added to PlayingCard class

--- src/main/java/Oblig3/DeckOfCards/PlayingCard.java
+++ src/main/java/Oblig3/DeckOfCards/PlayingCard.java
@@ -4,4 +4,17 @@ public class PlayingCard {
     private Suit suit;
     private Face face;
 
+    public PlayingCard(Suit suit, Face face) {
+        this.suit = suit;
+        this.face = face;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Face getFace() {
+        return face;
+    }
+
 }
